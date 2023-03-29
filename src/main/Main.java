18c8e58cdd5bf300d05cd792309120293741a78b
nurdolotmamasabirov <nurdolotmamasabirov225@gmail.com>
2023-03-29 17:13:47 +0600

    update commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,8 @@
 package main;
 
 public class Main {
-<<<<<<<<< Temporary merge branch 1
+
     public static void main(String[] args) {
-=========
-    public static void main(String[] args){
->>>>>>>>> Temporary merge branch 2
 
     }
 }
